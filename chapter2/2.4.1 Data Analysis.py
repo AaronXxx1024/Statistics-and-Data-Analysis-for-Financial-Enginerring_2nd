70e33d201e94c04356b9720bddffded94a862336
Han Xiao (Aaron) <49572168+AaronXxx1024@users.noreply.github.com>
2021-02-28 21:23:25 +0800

    Update 2.4.1 Data Analysis.py

--- chapter2/2.4.1 Data Analysis.py
+++ chapter2/2.4.1 Data Analysis.py
@@ -107,3 +107,105 @@ for i in range(100000):
         below[i] = 1
 
 print(np.mean(below))
+
+#%%
+"""
+Problem 5 
+---------
+What is the probability that the hedge fund will make a profit of at least $100,000? 
+P(profit >= $100,00) =  0.38855
+---------
+
+Problem 6 
+---------
+What is the probability the hedge fund will suffer a loss? 
+P(suffer a loss) =  0.58622
+---------
+
+Problem 7 
+---------
+What is the expected profit from this trading strategy? 
+Expected Profit:  10161.415322615974
+---------
+
+Problem 8 
+---------
+What is the expected return? When answering this question, remember that only $50,000 was invested. 
+Also, the units of return are time, e.g., one can express a return as a daily return or a weekly return. 
+Therefore, one must keep track of how long the hedge fund holds its position before selling.
+---------
+Expected Profit:  0.0020322830645231947
+"""
+
+np.random.seed(2021)
+
+below = [0] * int(1e5)
+above = [0] * int(1e5)
+middle = [0] * int(1e5)
+profit = [0] * int(1e5)
+
+for i in range(int(1e5)):
+    # Create an normally distributed random variable ~ N(0.05, 0.23**2)
+    r = 0.05/253 + 0.23/math.sqrt(253) * np.random.randn(100)
+    current_ret = 0
+    profit[i] = np.exp(np.log(1e6) + sum(r)) - 1e6
+    for ret in r:
+        current_ret += ret
+        log_price = np.log(1e6) + current_ret
+        if log_price <= np.log(950000):
+            below[i] = 1
+            profit[i] = -50000
+            break
+        elif log_price >= np.log(1100000):
+            above[i] = 1
+            profit[i] = 100000
+            break
+        middle[i] = 1
+
+expected_return = (np.array(profit) / 50000.0) / 100
+
+print("P(profit >= $100,00) = " , np.mean(above))
+print("P(suffer a loss) = " , np.mean(below))
+print("P(price between $950,000 & $1,100,000) =  ", np.mean(middle))
+print("Expected Profit: ", np.mean(profit))
+print("Expected Profit: ", np.mean(expected_return))
+
+#%%
+"""
+Problem 9 
+---------
+In this simulation, what are the mean and standard deviation of the log-returns for 1 year? 
+mean: 0.001206613351199386
+std: 0.012675694201796584
+---------
+
+Problem 10 
+---------
+Discuss how the price series appear to have momentum. Is the appearance of momentum real or an illusion? 
+illusion
+---------
+
+Problem 11 
+---------
+Explain what the code c(120,120*exp(cumsum(logr))) does?
+create a price series movement started from 120, total length is 254.
+---------
+
+"""
+
+np.random.seed(2021)
+
+price = []
+logr = 0.05/253 + 0.2/math.sqrt(253) * np.random.randn(253)
+current_r = 0
+for r in logr:
+    current_r += r
+    price.append(np.exp(current_r) * 120)
+
+pd.Series(price).plot()
+plt.show()
+
+print(np.mean(logr))
+print(np.std(logr))
+
+
